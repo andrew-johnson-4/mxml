d6a67ac16f5e472e0c622b39f19b2ebfe2cb42fe
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-06-20 13:40:45 -0700

    parser is about half done

--- src/mixin.rs
+++ src/mixin.rs
@@ -5,20 +5,100 @@
 
 use quote::{quote_spanned, ToTokens};
 use syn::parse::{Parse, ParseStream, Result};
-use syn::{parenthesized,Ident,token,Token};
+use syn::{parenthesized,Ident,token,Token,LitStr};
 use syn::punctuated::Punctuated;
 
-/*
-mixin!(Tooltip(message),
-  <? +"data-toggle"="tooltip" +"data-placement"="top" +title={{message}}/>
-);
-*/
+pub enum AttrPrefix {
+   Match(Token![~]),
+   Add(Token![+]),
+}
+impl Parse for AttrPrefix {
+   fn parse(input: ParseStream) -> Result<Self> {
+      Ok(if input.peek(Token![~]) {
+         AttrPrefix::Match(input.parse()?)
+      } else {
+         AttrPrefix::Add(input.parse()?)
+      })
+   }
+}
+
+pub struct Attr {
+   _attr_prefix: AttrPrefix,
+   _key: IdentOrStr,
+   _eq: Token![=],
+   _val: LitStr
+}
+impl Parse for Attr {
+   fn parse(input: ParseStream) -> Result<Self> {
+      Ok(Attr {
+         _attr_prefix: input.parse()?,
+         _key: input.parse()?,
+         _eq: input.parse()?,
+         _val: input.parse()?
+      })
+   }
+}
+impl Attr {
+   fn parse_outer(input: ParseStream) -> Result<Vec<Self>> {
+      let mut many = Vec::new();
+      while input.peek(Token![~]) || input.peek(Token![+]) {
+         many.push(input.parse()?);
+      }
+      Ok(many)
+   }
+}
+
+pub enum IdentOrStr {
+   Ident(Ident),
+   Str(LitStr)
+}
+impl Parse for IdentOrStr {
+   fn parse(input: ParseStream) -> Result<Self> {
+      Ok(if input.peek(LitStr) {
+         IdentOrStr::Str(input.parse()?)
+      } else {
+         IdentOrStr::Ident(input.parse()?)
+      })
+   }
+}
+
+pub enum IdentOrAny {
+   Ident(Ident),
+   Any(Token![?])
+}
+impl Parse for IdentOrAny {
+   fn parse(input: ParseStream) -> Result<Self> {
+      Ok(if input.peek(Token![?]) {
+         IdentOrAny::Any(input.parse()?)
+      } else {
+         IdentOrAny::Ident(input.parse()?)
+      })
+   }
+}
+
+pub struct FME {
+   _open_bracket: Token![<],
+   _name: IdentOrAny,
+   _attrs: Vec<Attr>,
+   _close_bracket: Token![>]
+}
+impl Parse for FME {
+   fn parse(input: ParseStream) -> Result<Self> {
+      Ok(FME {
+         _open_bracket: input.parse()?,
+         _name: input.parse()?,
+         _attrs: Attr::parse_outer(input)?,
+         _close_bracket: input.parse()?
+      })
+   }
+}
 
 pub struct Mixin {
    name: Ident,
    _paren_token: token::Paren,
    _args: Punctuated<Ident, Token![,]>,
    _comma: Token![,],
+   _fme: FME
 }
 impl Parse for Mixin {
    fn parse(input: ParseStream) -> Result<Self> {
@@ -27,7 +107,8 @@ impl Parse for Mixin {
          name: input.parse()?,
          _paren_token: parenthesized!(content in input),
          _args: content.parse_terminated(Ident::parse)?,
-         _comma: input.parse()?
+         _comma: input.parse()?,
+         _fme: input.parse()?
       })
    }
 }
